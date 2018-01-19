5d715403831e0aca6e27b4917ce34ac7c544c23c
won song <wonsong82@gmail.com>
2018-01-18 19:50:04 -0500

    index

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+<head>
+<title>autoupbit</title>
+</head>
+<body>
+<h1>AUTOUPBIT</h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,8 @@
 <?php
+Route::get('/', function(){
+	return view('welcome');	
+});
+
 Route::get('price', 'PriceController@index');
 
 
