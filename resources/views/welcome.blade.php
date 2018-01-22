1ecabaf569d335730b95e3daaba6ee974133b880
won song <wonsong82@gmail.com>
2018-01-21 19:38:13 -0500

    added page selector

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,5 +5,18 @@
 </head>
 <body>
 <h1>AUTOUPBIT</h1>
+
+@php(
+$coins = ['BTC','XRP','ETH','BCC','NEO','ADA','LTC','ETC','ZEC','OMG','BTG','XMR','DASH'];
+)
+
+
+<ul>
+@foreach($coins as $coin)
+    <li><a href="/?USDT-{{$coin}}" target="_blank">USDT-{{$coin}}</a></li>
+    <li><a href="/?KRW-{{$coin}}" target="_blank">KRW-{{$coin}}</a></li>
+@endforeach
+</ul>
+
 </body>
 </html>
