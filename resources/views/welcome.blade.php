7337e7eb46612dbdeb69c4457b182fa05c8c3af2
won song <wonsong82@gmail.com>
2018-06-26 12:20:59 -0400

    ad

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,14 +7,14 @@
 <h1>AUTOUPBIT</h1>
 
 @php
-$coins = ['BTC','XRP','ETH','BCC','NEO','ADA','LTC','ETC','ZEC','OMG','BTG','XMR','DASH'];
+$coins = App\Models\Currency::where('is_active', true)->orderBy('order')->get();
 @endphp
 
 
 <ul>
 @foreach($coins as $coin)
-    <li><a href="/?USDT-{{$coin}}">USDT-{{$coin}}</a></li>
-    <li><a href="/?KRW-{{$coin}}">KRW-{{$coin}}</a></li>
+    <li><a href="/?USDT-{{$coin->code}}">USDT-{{$coin->code}}</a></li>
+    <li><a href="/?KRW-{{$coin->code}}">KRW-{{$coin->code}}</a></li>
 @endforeach
 </ul>
 
